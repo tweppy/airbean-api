390e05c619017aa5304bb9808e678b821ed37432
tweppy <tweppy@gmail.com>
2023-04-03 11:56:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const PORT = process.env.PORT || 8000;
 const menuData = require("./menu.json");
 app.use(express.json());
 
-app.get("/api/menu", (req, res) => {
+app.get("/api/beans", (req, res) => {
   res.json(menuData);
 });
 
 app.listen(PORT, () => {
   console.log("Server started");
 });
+
+/*
+beans
+/api/beans/                         - GET menu
+/api/beans/order                    - POST lägg till order
+/api/beans/order/status/:orderNr    - GET pågående beställning inloggad eller gäst
+
+user
+/api/user/signup         - POST skapa konto
+/api/user/login          - POST logga in
+/api/user/history        - GET inloggad orderhistorik
+*/
