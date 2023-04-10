ea1c3618997453d4f21594c9b6fa365715d9165e
tweppy <tweppy@gmail.com>
2023-04-10 10:05:34 +0200

    minor clean up

--- controllers/beans/orderController.js
+++ controllers/beans/orderController.js
@@ -7,14 +7,12 @@ const {
 const { getTotalSum, createETA } = require('../../utils');
 const { v4: uuidv4 } = require('uuid');
 
-//get
 async function get(req, res) {
   const order = await getOrder();
   const total = await getTotalSum();
   res.json({ success: true, order: order, total: total });
 }
 
-//add
 async function add(req, res) {
   const { title, price } = req.body;
 
@@ -36,7 +34,6 @@ async function add(req, res) {
   res.json(result);
 }
 
-//remove
 async function remove(req, res) {
   const id = req.params.id;
   const result = await removeItem(id);
